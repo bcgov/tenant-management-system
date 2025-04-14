312846e2849e83157d4dffb5eae86c12ec5b2269
Walter Moar <Walter.Moar@gov.bc.ca>
2025-04-14 15:55:55 -0700

    refactor: TMS-2257 replace frontend with default Vite / Vue / TypeScript app (#3)
    * refactor: TMS-2257 replace default frontend with Vue3 + TypeScript
    
    * refactor: TMS-2257 redo Vue3 + TypeScript to use Vite
    
    * refactor: TMS-2257 limit to 80 cols and add the package-lock

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,19 +1,14 @@
-import { defineConfig } from 'vitest/config'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { fileURLToPath } from 'node:url'
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import viteConfig from './vite.config'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [tsconfigPaths()],
-  test: {
-    exclude: ['**/node_modules/**', '**/e2e/**'],
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: 'src/test-setup.ts',
-    // you might want to disable it, if you don't have tests that rely on CSS
-    // since parsing CSS is slow
-    css: false,
-    coverage: {
-      reporter: ['lcov', 'text-summary', 'text', 'json', 'html'],
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/**'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
     },
-  },
-})
+  }),
+)
