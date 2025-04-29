9dc1dc31508ca7fffcab891e5826dea026eaf40b
Walter Moar <Walter.Moar@gov.bc.ca>
2025-04-29 17:44:18 +0000

    fix: TMS-2207 layout tweak for page whitespace
    Add a margin to the bottom of the navigation bar to provide some whitespace above the page.

--- frontend/src/components/layout/AppNavigation.vue
+++ frontend/src/components/layout/AppNavigation.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-toolbar class="px-10" flat elevation="0">
+  <v-toolbar class="px-10 mb-12" flat elevation="0">
     <v-btn to="/tenants" variant="text">Tenants</v-btn>
     <v-btn to="/roles" variant="text">My Roles</v-btn>
   </v-toolbar>
