51aaca98cf5a235898f3b6d71b8f6594d3f91e88
Walter Moar <Walter.Moar@gov.bc.ca>
2025-05-02 08:28:12 -0700

    refactor: TIMS-2207 update keycloak init to be async (#18)
    * refactor: TMS-2207 convert init keycloak to async
    
    * revert: TMS-2207 remove generated comments

--- frontend/src/components/BaseSecure.vue
+++ frontend/src/components/BaseSecure.vue
@@ -1,25 +1,20 @@
 <script setup lang="ts">
-// Import necessary functions and refs from Vue and Keycloak service
 import { ref, onMounted } from 'vue'
+
 import { isLoggedIn as checkIsLoggedIn, login } from '@/services/keycloak'
 
-// Create a reactive reference to track authentication status
 const isAuthenticated = ref(false)
 
-// When the component is mounted, check if the user is logged in
 onMounted(() => {
   if (checkIsLoggedIn()) {
-    // If the user is logged in, set isAuthenticated to true
     isAuthenticated.value = true
   } else {
-    // Optionally redirect to login page if not authenticated
     login()
   }
 })
 </script>
 
 <template>
-  <!-- Render child components only if the user is authenticated -->
   <div v-if="isAuthenticated">
     <slot></slot>
   </div>
