9cb3e4c631b07012ccffc44186661cf7e89a36fd
Walter Moar <Walter.Moar@gov.bc.ca>
2025-05-02 16:45:28 +0000

    refactor: TMS-2208 clean up create tenant code and ui
    Fix the navigation bar to provide horizontal space between the buttons.

--- frontend/src/components/layout/AppNavigation.vue
+++ frontend/src/components/layout/AppNavigation.vue
@@ -1,6 +1,8 @@
 <template>
-  <v-toolbar class="px-10 mb-12" flat elevation="0">
-    <v-btn to="/tenants" variant="text">Tenants</v-btn>
-    <v-btn to="/roles" variant="text">My Roles</v-btn>
+  <v-toolbar class="px-12 mb-12" flat elevation="0">
+    <div class="d-flex align-center" style="gap: 8px">
+      <v-btn to="/tenants" variant="text">Tenants</v-btn>
+      <v-btn to="/roles" variant="text">My Roles</v-btn>
+    </div>
   </v-toolbar>
 </template>
