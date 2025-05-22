aaf41e87ea934d607f886e32617eaef7fa495c75
jasonchung1871 <101672465+jasonchung1871@users.noreply.github.com>
2025-05-22 02:13:49 -0700

    refactor: Refactor backend configuration (#26)
    * Refactor backend configuration
    
    Backend should be configured to conform with the pipelines crunchy configuration that is generated.
    
    * Update Dockerfile
    
    docker build is saying that NPM_PACKAGES and NODE_PATH is undefined
    
    * Update Dockerfile
    
    removed undefined variable usage

--- backend/src/ormconfig.ts
+++ backend/src/ormconfig.ts
@@ -1,31 +1,29 @@
 import * as dotenv from "dotenv";
 dotenv.config();
-
-const isTestEnv = process.env.NODE_ENV === "test"
  
-  const dbConfig = {
-    type: 'postgres',
-    host: isTestEnv? 'localhost' : process.env.DB_HOST,
-    port: isTestEnv? 54321: process.env.DB_PORT,
-    username: isTestEnv? 'testuser': process.env.DB_USER,
-    password: isTestEnv? 'testpassword': process.env.DB_PASSWORD,
-    database: isTestEnv? 'testdb': process.env.DB_DATABASE,
-    synchronize: false,
-    logging: isTestEnv,
-    entities: [
-      'src/entities/**/*.ts'
-    ],
-    migrations: [
-      './src/migrations/*.ts'
-    ],
-    subscribers: [
-      'src/subscriber/**/*.ts'
-    ],
-    cli: {
-      entitiesDir: 'src/entities',
-      migrationsDir: 'src/migration',
-      subscribersDir: 'src/subscriber'
-    }
- }
+const dbConfig = {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
+  username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'mysecretpassword',
+  database: process.env.POSTGRES_DATABASE || 'postgres',
+  synchronize: false,
+  logging: true,
+  entities: [
+    'src/entities/**/*.ts'
+  ],
+  migrations: [
+    './src/migrations/*.ts'
+  ],
+  subscribers: [
+    'src/subscriber/**/*.ts'
+  ],
+  cli: {
+    entitiesDir: 'src/entities',
+    migrationsDir: 'src/migration',
+    subscribersDir: 'src/subscriber'
+  }
+}
 
 module.exports = dbConfig
