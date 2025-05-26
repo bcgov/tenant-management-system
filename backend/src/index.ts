541bfda126534e4596a37dba50b700c3057cc9f7
Shankar Sethuraman <54600222+shankar-aot@users.noreply.github.com>
2025-05-26 14:15:49 -0600

    updates to openapi spec to reflect new api updates, moved mw ordering of docs to support new security updates

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,18 +2,9 @@ import App  from './app'
 require('dotenv').config()
 
 const app = new App().app
-
-const swaggerUi = require("swagger-ui-express");
-const YAML = require("yamljs");
-const path = require("path");
-
 const PORT = process.env.PORT || 4144
 
-const swaggerDocument = YAML.load(path.join(__dirname, "docs", "openapi.yaml"))
-
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-    app.listen(PORT, () => {
-      console.log('TMS API is now available on port: ' + PORT)
-      console.log(`TMS API docs available at http://localhost:${PORT}/docs`);
-    })
+app.listen(PORT, () => {
+  console.log('TMS API is now available on port: ' + PORT)
+  console.log(`TMS API docs available at http://localhost:${PORT}/docs`);
+})
