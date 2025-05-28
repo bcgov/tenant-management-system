f61571b5d307f695910d98d6470adaca902f8d72
Shankar Sethuraman <54600222+shankar-aot@users.noreply.github.com>
2025-05-27 19:02:12 -0600

    Feat/api updates (#40)
    * fixed validation error during create tenant and updated env sample to reflect latest env vars
    
    * removed auth check as this method pulls tenants for users
    
    * update to tenant model - added description field, db migrations, repo updates, validation updates, docs updates

--- backend/src/common/tms.validator.ts
+++ backend/src/common/tms.validator.ts
@@ -6,6 +6,7 @@ export default {
         body: Joi.object({
             name: Joi.string().min(1).max(30).required(),
             ministryName: Joi.string().min(1).max(100).required(),
+            description: Joi.string().min(1).max(500).optional(),
             user: Joi.object().keys({
                 firstName: Joi.string().min(1).max(50).required(),
                 lastName: Joi.string().min(1).max(50).required(),
---
+++ backend/src/migrations/1748393596089-tenant-description.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class TenantDescription1748393596089 implements MigrationInterface {
+    name = 'TenantDescription1748393596089'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "Tenant" ADD "description" character varying(500) DEFAULT 'Default Tenant Description'`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "Tenant" DROP COLUMN "description"`);
+    }
+
+}
