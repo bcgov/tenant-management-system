a5990be467af7a55e3f1438d9d886093a36b5d47
Walter Moar <Walter.Moar@gov.bc.ca>
2025-06-03 21:19:56 +0000

    build: TMS-2210 update vuetify
    Updated Vuetify to latest in the hope of fixing a bug but no such luck.

--- frontend/src/components/tenant/TenantTabs.vue
+++ frontend/src/components/tenant/TenantTabs.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+
+import TenantUserManagement from '@/components/tenant/TenantUserManagement.vue'
 import type { Tenant } from '@/models/tenant.model'
 
 defineProps<{
@@ -7,28 +9,32 @@ defineProps<{
   tenant?: Tenant
 }>()
 
-const tab = ref<number | null>(null)
+const tab = ref<number>(0)
 </script>
 
 <template>
   <v-card elevation="0">
     <v-tabs v-model="tab" :mandatory="false" :disabled="disabled">
+      <v-tab :value="0" class="pa-0 ma-0" style="min-width: 0px" />
       <v-tab :value="1">User Management</v-tab>
       <v-tab :value="2">Available Services</v-tab>
     </v-tabs>
 
-    <v-tabs-window v-model="tab">
-      <v-tabs-window-item :value="1">
-        <v-container fluid>
-          <v-row>
-            <v-col cols="12">
-              <p>Content for User Management tab</p>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-tabs-window-item>
+    <v-window v-model="tab">
+      <v-window-item :value="0">
+        <!--
+          Vuetify insists that a tab is always active on page load. This
+          invisible tab and content will be shown until the user selects one of
+          the real tabs.
+          https://stackoverflow.com/questions/58355920/vuetify-deselecting-v-tabs
+         -->
+      </v-window-item>
 
-      <v-tabs-window-item :value="2">
+      <v-window-item :value="1">
+        <TenantUserManagement :tenant="tenant" />
+      </v-window-item>
+
+      <v-window-item :value="2">
         <v-container fluid>
           <v-row>
             <v-col cols="12">
@@ -36,7 +42,7 @@ const tab = ref<number | null>(null)
             </v-col>
           </v-row>
         </v-container>
-      </v-tabs-window-item>
-    </v-tabs-window>
+      </v-window-item>
+    </v-window>
   </v-card>
 </template>
