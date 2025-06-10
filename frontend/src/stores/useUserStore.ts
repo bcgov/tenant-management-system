334c5b705f439346780506107ccfed4c0ce4a369
Walter Moar <Walter.Moar@gov.bc.ca>
2025-06-10 22:24:33 +0000

    feat: TMS-2209 map user search
    Now using the response mapping to create a proper User. Removed the tenant code as it's not needed anymore.

--- frontend/src/stores/useUserStore.ts
+++ frontend/src/stores/useUserStore.ts
@@ -1,44 +1,27 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
-import { useAuthStore } from '@/stores/useAuthStore'
+
 import { useNotification } from '@/composables/useNotification'
+import { User } from '@/models/user.model'
 import { userService } from '@/services/user.service'
-import type { Tenant } from '@/models/tenant.model'
-import type { User } from '@/models/user.model'
 import type { IdirSearchParameters } from '@/types/IdirSearchParameters'
 
 export const useUserStore = defineStore('user', () => {
-  const userTenants = ref<Tenant[]>([])
   const searchResults = ref<User[]>([])
   const loading = ref(false)
 
   const { addNotification } = useNotification()
 
-  async function fetchUserTenants() {
-    const authStore = useAuthStore()
-    if (!authStore.user?.id) {
-      addNotification('User ID is not available', 'error')
-      throw new Error('User ID is not available')
-    }
-
-    loading.value = true
-    try {
-      userTenants.value = await userService.getUserTenants(authStore.user.id)
-    } catch (error) {
-      addNotification('Error fetching user tenants', 'error')
-      throw error
-    } finally {
-      loading.value = false
-    }
-  }
-
   async function searchIdirUsers(params: IdirSearchParameters) {
     loading.value = true
     try {
-      searchResults.value = await userService.searchIdirUsers(params)
+      const response = await userService.searchIdirUsers(params)
+      searchResults.value = response.map(User.fromSearchData)
+
       return searchResults.value
     } catch (error) {
       addNotification('Error searching IDIR users', 'error')
+
       throw error
     } finally {
       loading.value = false
@@ -46,10 +29,9 @@ export const useUserStore = defineStore('user', () => {
   }
 
   return {
-    userTenants,
-    searchResults,
     loading,
-    fetchUserTenants,
+    searchResults,
+
     searchIdirUsers,
   }
 })
