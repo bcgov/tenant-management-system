f517b8b27035f2f6221b6195ea41912cd953226b
Walter Moar <Walter.Moar@gov.bc.ca>
2025-06-10 22:23:38 +0000

    refactor: TMS-2209 tenant removal
    Removed the tenant lookup function from the service, as it's now supplied by the tenant service.

--- frontend/src/services/user.service.ts
+++ frontend/src/services/user.service.ts
@@ -1,8 +1,6 @@
-import type { Tenant } from '@/models/tenant.model'
-import type { User } from '@/models/user.model'
-import type { IdirSearchParameters } from '@/types/IdirSearchParameters'
 import { authenticatedAxios } from '@/services/authenticated.axios'
 import { config } from '@/services/config.service'
+import type { IdirSearchParameters } from '@/types/IdirSearchParameters'
 import { logger } from '@/utils/logger'
 
 const userApi = authenticatedAxios()
@@ -10,33 +8,21 @@ userApi.defaults.baseURL = config.api.baseUrl
 
 export const userService = {
   /**
-   * Gets the tenants for a specific user.
-   * @param {string} userId - The ID of the user.
-   * @returns {Promise<Tenant[]>} A promise that resolves to an array of tenants.
-   */
-  async getUserTenants(userId: string): Promise<Tenant[]> {
-    try {
-      const response = await userApi.get(`/users/${userId}/tenants`)
-      return response.data.data
-    } catch (error) {
-      logger.error('Error fetching user tenants:', error)
-      throw error
-    }
-  },
-
-  /**
    * Searches for IDIR users based on the provided parameters.
    * @param {IdirSearchParameters} params - The search parameters.
-   * @returns {Promise<User[]>} A promise that resolves to an array of users.
+   * @returns {Promise<object[]>} A promise that resolves to an array of
+   *   user-like objects.
    */
-  async searchIdirUsers(params: IdirSearchParameters): Promise<User[]> {
+  async searchIdirUsers(params: IdirSearchParameters) {
     try {
       const response = await userApi.get('/users/bcgovssousers/idir/search', {
         params,
       })
+
       return response.data.data
     } catch (error) {
       logger.error('Error searching IDIR users:', error)
+
       throw error
     }
   },
