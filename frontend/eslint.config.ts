91c2cb8335420f3023ba61f08acb011278641e8c
Walter Moar <Walter.Moar@gov.bc.ca>
2025-07-02 23:34:03 +0000

    feat: TMS-2508 add vue linting rule
    Update the linter to add rules and start to fix the results.

--- frontend/eslint.config.ts
+++ frontend/eslint.config.ts
@@ -1,10 +1,15 @@
 import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import {
+  defineConfigWithVueTs,
+  vueTsConfigs,
+} from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
 import pluginVitest from '@vitest/eslint-plugin'
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import pluginCypress from 'eslint-plugin-cypress/flat'
+import tsParser from '@typescript-eslint/parser'
+import vueParser from 'vue-eslint-parser'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
@@ -21,19 +26,58 @@ export default defineConfigWithVueTs(
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
   pluginVue.configs['flat/essential'],
-  vueTsConfigs.recommended,
-  
+  pluginVue.configs['flat/strongly-recommended'],
+  pluginVue.configs['flat/recommended'],
+  vueTsConfigs.strict,
+
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    // rules: {
+    //   'vue/attributes-order': [
+    //     'error',
+    //     {
+    //       order: [
+    //         'DEFINITION',
+    //         'LIST_RENDERING',
+    //         'CONDITIONALS',
+    //         'RENDER_MODIFIERS',
+    //         'GLOBAL',
+    //         'UNIQUE',
+    //         'SLOT',
+    //         'TWO_WAY_BINDING',
+    //         'OTHER_DIRECTIVES',
+    //         'ATTR_DYNAMIC',
+    //         'ATTR_STATIC',
+    //         'ATTR_SHORTHAND_BOOL',
+    //         'EVENTS',
+    //         'CONTENT',
+    //       ],
+    //       alphabetical: true,
+    //     },
+    //   ],
+    // },
+  },
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
-  
+
   {
     ...pluginCypress.configs.recommended,
     files: [
       'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-      'cypress/support/**/*.{js,ts,jsx,tsx}'
+      'cypress/support/**/*.{js,ts,jsx,tsx}',
     ],
   },
+
   skipFormatting,
 )
