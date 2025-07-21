923b8d93d51f577811afca7c0e98fa58e062c534
Walter Moar <Walter.Moar@gov.bc.ca>
2025-07-21 23:35:47 +0000

    feat(ui): TMS-2244 create tenant group
    First feature for creating groups for a tenant.

--- frontend/src/models/index.ts
+++ frontend/src/models/index.ts
@@ -1,3 +1,4 @@
+export { Group, type GroupDetailFields } from './group.model'
 export { Role } from './role.model'
 export { Tenant, type TenantDetailFields } from './tenant.model'
 export { User } from './user.model'
--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,3 +1,4 @@
+export { groupService } from './group.service'
 export { roleService } from './role.service'
 export { tenantService } from './tenant.service'
 export { userService } from './user.service'
