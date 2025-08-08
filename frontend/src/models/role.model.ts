4ff05234c2d3e013a1ac1a1fe118d51ef60bde83
Walter Moar <Walter.Moar@gov.bc.ca>
2025-08-08 15:55:51 +0000

    docs(ui): TMS-2680 model documentation
    Added the missing documentation for models.

--- frontend/src/models/role.model.ts
+++ frontend/src/models/role.model.ts
@@ -1,14 +1,44 @@
+/**
+ * Represents a role within the system.
+ */
 export class Role {
+  /**
+   * Description of the role.
+   */
   description: string
+
+  /**
+   * Unique identifier for the role.
+   */
   id: string
+
+  /**
+   * Name of the role.
+   */
   name: string
 
+  /**
+   * Creates a new Role instance.
+   *
+   * @param id - Unique identifier for the role
+   * @param name - Name of the role
+   * @param description - Description of the role
+   */
   constructor(id: string, name: string, description: string) {
     this.description = description
     this.id = id
     this.name = name
   }
 
+  /**
+   * Creates a Role instance from API response data.
+   *
+   * @param apiData - The raw role data from the API
+   * @param apiData.description - Description of the role
+   * @param apiData.id - Unique identifier for the role
+   * @param apiData.name - Name of the role
+   * @returns A new Role instance
+   */
   static fromApiData(apiData: {
     description: string
     id: string
