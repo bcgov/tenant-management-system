7728bba9aff4617040fdde27d8ebc85ee0150b18
abhilash-aot <abhilash.kr@aot-technologies.com>
2025-11-26 13:08:40 -0800

    Reverted back sql

--- migrations/sql/V1.0.0__init.sql
+++ migrations/sql/V1.0.0__init.sql
@@ -1,21 +1,21 @@
-CREATE SCHEMA IF NOT EXISTS tms;
+CREATE SCHEMA IF NOT EXISTS USERS;
 
-CREATE SEQUENCE IF NOT EXISTS tms."USER_SEQ"
+CREATE SEQUENCE IF NOT EXISTS USERS."USER_SEQ"
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
     CACHE 100;
 
-CREATE TABLE IF NOT EXISTS tms.USERS
+CREATE TABLE IF NOT EXISTS USERS.USERS
 (
     ID    numeric      not null
         constraint "USER_PK"
-            primary key DEFAULT nextval('tms."USER_SEQ"'),
+            primary key DEFAULT nextval('USERS."USER_SEQ"'),
     NAME  varchar(200) not null,
     EMAIL varchar(200) not null
 );
-INSERT INTO tms.USERS (NAME, EMAIL)
+INSERT INTO USERS.USERS (NAME, EMAIL)
 VALUES ('John', 'John.ipsum@test.com'),
        ('Jane', 'Jane.ipsum@test.com'),
        ('Jack', 'Jack.ipsum@test.com'),
