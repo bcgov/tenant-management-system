4285f2ae256a6fbdd29fa73ac7e3190a23313c84
BrandonSharratt <brandon@highwaythreesolutions.com>
2025-12-09 17:01:58 -0800

    auto select first tabs (#258)

--- frontend/src/components/route/SettingsContainer.vue
+++ frontend/src/components/route/SettingsContainer.vue
@@ -15,21 +15,16 @@ const tab = ref<number>(0)
     <AdministratorContainer>
       <v-card elevation="0">
         <v-tabs v-model="tab" :mandatory="false">
-          <!-- The v-tabs component insists on always having an active tab. Use
-           an invisible Tab 0 to make v-tabs happy. -->
-          <v-tab :value="0" class="pa-0 ma-0" style="min-width: 0px" />
-          <v-tab :value="1">Tenant Requests</v-tab>
-          <v-tab :value="2">Shared Services</v-tab>
+          <v-tab :value="0">Tenant Requests</v-tab>
+          <v-tab :value="1">Shared Services</v-tab>
         </v-tabs>
 
         <v-window v-model="tab">
-          <v-window-item :value="0" />
-
-          <v-window-item :value="1">
+          <v-window-item :value="0">
             <TenantRequestContainer />
           </v-window-item>
 
-          <v-window-item :value="2">
+          <v-window-item :value="1">
             <v-container fluid>
               <v-row>
                 <v-col cols="12">
