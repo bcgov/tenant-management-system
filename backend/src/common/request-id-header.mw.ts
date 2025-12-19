9b955b287feccc476704a9ea2e504c6b8b2d0ba8
Shankar Sethuraman <54600222+shankar-aot@users.noreply.github.com>
2025-12-19 13:29:40 -0700

    new mw to add X-Request-Id header to all responses for tracking

---
+++ backend/src/common/request-id-header.mw.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express';
+import rTracer from 'cls-rtracer';
+
+
+export const addRequestIdHeader = (req: Request, res: Response, next: NextFunction): void => {
+  const requestId = rTracer.id();
+
+  if (requestId && typeof requestId === 'string') {
+    const originalSend = res.send;
+    res.send = function(body: any) {
+      res.setHeader('X-Request-Id', requestId);
+      return originalSend.call(this, body);
+    };
+
+    const originalJson = res.json;
+    res.json = function(body: any) {
+      res.setHeader('X-Request-Id', requestId);
+      return originalJson.call(this, body);
+    };
+  }
+
+  next();
+};
+
