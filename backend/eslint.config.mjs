1b1e41996370c7192f1293218db520b0d3c59288
Walter Moar <Walter.Moar@gov.bc.ca>
2026-02-02 23:19:46 +0000

    chore: CCP-3353 standardize linting
    Set up the linting config files so that they're as similar as possible.

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,73 @@
+import tsEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+// ---- Shared base rules ----
+const baseRules = {
+  // Code quality
+  curly: ["error", "all"],
+  eqeqeq: ["error", "always", { null: "ignore" }],
+  "no-throw-literal": "error",
+  "prefer-promise-reject-errors": "error",
+
+  // General best practices
+  "no-console": ["warn", { allow: ["warn", "error"] }],
+  "no-debugger": "warn",
+  "no-var": "error",
+  "prefer-const": "error",
+
+  // Style
+  "comma-dangle": ["error", "always-multiline"],
+  quotes: ["error", "single", { avoidEscape: true }],
+  semi: ["error", "never"],
+
+  // TypeScript
+  "@typescript-eslint/explicit-function-return-type": "off",
+  "@typescript-eslint/explicit-module-boundary-types": "off",
+  "@typescript-eslint/no-explicit-any": "error",
+  "@typescript-eslint/no-non-null-assertion": "warn",
+  "@typescript-eslint/no-unused-vars": [
+    "error",
+    {
+      argsIgnorePattern: "^_",
+      caughtErrorsIgnorePattern: "^_",
+      varsIgnorePattern: "^_",
+    },
+  ],
+};
+
+// ---- ESLint flat config ----
+const config = [
+  // ignore build/output
+  {
+    ignores: [
+      "**/.git/**",
+      "**/build/**",
+      "**/coverage/**",
+      "**/dist/**",
+      "**/dist-ssr/**",
+      "**/node_modules/**",
+    ],
+  },
+
+  // TS files
+  {
+    files: ["**/*.{ts,mts,tsx}"],
+    languageOptions: {
+      globals: {
+        console: "readonly",
+        process: "readonly",
+        Buffer: "readonly",
+      },
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        project: "./tsconfig.json",
+        sourceType: "module",
+      },
+    },
+    plugins: { "@typescript-eslint": tsEslint },
+    rules: baseRules,
+  },
+];
+
+export default config;
