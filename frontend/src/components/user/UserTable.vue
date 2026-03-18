15766566f497e450bd82b4ab1adc2c0daab6c3d4
BrandonSharratt <brandon@highwaythreesolutions.com>
2026-03-18 19:25:12 +0000

    build fix

--- frontend/src/components/user/UserTable.vue
+++ frontend/src/components/user/UserTable.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
+import type { ComputedRef } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 import type { User, Tenant, Role, GroupUser } from '@/models'
