9eee58b19d9a95ecae12048f5b4ed06916cf3148
Walter Moar <Walter.Moar@gov.bc.ca>
2026-04-22 15:19:13 +0000

    refactor: CCP-4215 add tertiary button

---
+++ frontend/src/components/ui/ButtonTertiary.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+// --- Component Interface -----------------------------------------------------
+
+defineProps<{
+  disabled?: boolean
+  icon?: string
+  text: string
+}>()
+
+const emit = defineEmits<{
+  (event: 'click'): void
+}>()
+</script>
+
+<template>
+  <v-btn
+    :disabled="disabled"
+    :prepend-icon="icon"
+    class="cstar-button-tertiary"
+    variant="text"
+    @click="emit('click')"
+  >
+    {{ text }}
+  </v-btn>
+</template>
+
+<style scoped>
+.cstar-button-tertiary:disabled {
+  background-color: rgb(var(--v-theme-tertiary-disabled)) !important;
+}
+
+.cstar-button-tertiary:hover:not(:disabled) {
+  background-color: rgb(var(--v-theme-tertiary-hover)) !important;
+}
+</style>
