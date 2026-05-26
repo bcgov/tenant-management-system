5197a70ae3e5bced11cb41d6f7d8a13459453500
Walter Moar <Walter.Moar@gov.bc.ca>
2026-05-26 19:40:22 +0000

    feat: CCP-4535 service card layout

--- frontend/src/components/route/TenantServiceContainer.vue
+++ frontend/src/components/route/TenantServiceContainer.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
-import { useI18n } from 'vue-i18n'
 
 import ServiceManagement from '@/components/service/ServiceManagement.vue'
 import { useNotification } from '@/composables/useNotification'
@@ -8,8 +7,6 @@ import { type ServiceId } from '@/models/service.model'
 import { type TenantId } from '@/models/tenant.model'
 import { useServiceStore } from '@/stores/useServiceStore'
 import { useTenantStore } from '@/stores/useTenantStore'
-import { ROLES } from '@/utils/constants'
-import { currentUserHasRole } from '@/utils/permissions'
 
 // --- Component Interface -----------------------------------------------------
 
@@ -23,10 +20,6 @@ const notification = useNotification()
 const serviceStore = useServiceStore()
 const tenantStore = useTenantStore()
 
-// --- Component State ---------------------------------------------------------
-
-const { t } = useI18n()
-
 // --- Component Methods -------------------------------------------------------
 
 async function handleAddService(serviceId: ServiceId) {
@@ -38,14 +31,13 @@ async function handleAddService(serviceId: ServiceId) {
       (service) => service.id === serviceId,
     )
 
+    // TODO: what is the implication of this being false?
     if (addedService) {
-      tenantServices.value.push(addedService)
+      serviceStore.tenantServices.push(addedService)
+      notification.success(
+        `${addedService.displayName} has been added to this tenant.`,
+      )
     }
-
-    notification.success(
-      `An available ${t('general.servicesLabelLower', 1)} for this tenant ` +
-        `was successfully added.`,
-    )
   } catch {
     notification.error('Failed to add service to tenant')
   }
@@ -53,11 +45,11 @@ async function handleAddService(serviceId: ServiceId) {
 
 // --- Computed Values ---------------------------------------------------------
 
-const isAdmin = computed(() => {
-  return currentUserHasRole(tenant.value, ROLES.TENANT_OWNER.value)
-})
-
-const services = computed(() => serviceStore.services)
+const services = computed(() =>
+  [...serviceStore.services].sort((a, b) =>
+    a.displayName.localeCompare(b.displayName),
+  ),
+)
 
 const tenant = computed(() => {
   const tenant = tenantStore.getTenant(props.tenantId)
@@ -68,7 +60,11 @@ const tenant = computed(() => {
   return tenant
 })
 
-const tenantServices = computed(() => serviceStore.tenantServices)
+const tenantServices = computed(() =>
+  [...serviceStore.tenantServices].sort((a, b) =>
+    a.displayName.localeCompare(b.displayName),
+  ),
+)
 
 // --- Component Lifecycle -----------------------------------------------------
 
@@ -97,46 +93,7 @@ init()
 
 <template>
   <template v-if="initialized">
-    <v-container v-if="tenantServices.length === 0" class="fill-height mt-12">
-      <v-row v-if="isAdmin" class="center-align justify-center">
-        <v-col class="align-center d-flex flex-column" cols="auto">
-          <h1>Connected Services</h1>
-          <p class="p-large">
-            Connected Services allow your tenant to manage access and assign
-            service roles to users and groups.
-          </p>
-
-          <v-divider class="my-12" style="width: 100%" />
-
-          <h3 class="mb-2">No Connected Services added yet</h3>
-          <p>
-            Add Connected Services to your tenant to make service roles
-            available to groups.
-          </p>
-
-          <ul>
-            <li>Select a Connected Service from the dropdown</li>
-            <li>Click Add Connected Service</li>
-            <li>Go to the Service Roles page to assign roles to groups</li>
-          </ul>
-        </v-col>
-      </v-row>
-      <v-row v-else class="center-align justify-center">
-        <v-col class="align-center d-flex flex-column" cols="auto">
-          <h1>No Connected Services available</h1>
-          <span class="p-large">
-            There are currently no Connected Services available to you in this
-            tenant.
-          </span>
-          <span class="p-large">
-            If you believe you should have access to a service, contact your
-            Tenant Owner.
-          </span>
-        </v-col>
-      </v-row>
-    </v-container>
     <ServiceManagement
-      v-else
       :services="services"
       :tenant="tenant!"
       :tenant-services="tenantServices"
---
+++ frontend/src/components/service/TenantServiceList.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import TenantServiceListCard from '@/components/service/TenantServiceListCard.vue'
+import { type Service } from '@/models/service.model'
+
+// --- Component Interface -----------------------------------------------------
+
+defineProps<{
+  tenantServices: Service[]
+}>()
+</script>
+
+<template>
+  <v-row>
+    <v-col
+      v-for="tenantService in tenantServices"
+      :key="tenantService.id"
+      cols="12"
+      md="3"
+    >
+      <TenantServiceListCard :service="tenantService" />
+    </v-col>
+  </v-row>
+</template>
