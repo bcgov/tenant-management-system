0795790f6663e78d421fcd77538433774a1cdf9a
Nimya John <78105113+nimya-aot@users.noreply.github.com>
2026-07-31 09:44:11 -0700

    test: CCP-4941 Add test for tenant request (#630)
    * Added test for tenant request
    
    * Updated test name
    
    * Removed Hard coded Ministry names

--- tests/e2e/playwright_tests/cstar_home.spec.ts
+++ tests/e2e/playwright_tests/cstar_home.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect, Page } from '@playwright/test'
 import { login } from '../support/login'
+import { MINISTRIES } from '../../../frontend/src/utils/constants'
 
 let sharedPage: Page
 
@@ -27,5 +28,36 @@ test.describe.serial('Landing page tests', () => {
   test('Checks the navigation links', async () => {
     await expect(sharedPage.getByText('All Tenants')).toBeVisible()
     await expect(sharedPage.getByText('Request a Tenant')).toBeVisible()
+  })
+
+  test('Submit tenant request under a Ministry', async () => {
+    await sharedPage.getByText('Request a Tenant').click()
+    const tenantName = sharedPage.getByLabel('Name of Tenant')
+    const tenantNameValue = `Test Tenant ${Date.now()}`
+    await tenantName.fill(tenantNameValue)
+    await expect(
+      sharedPage.locator('[data-testid="button-secondary"]'),
+    ).toContainText('Cancel')
+    await expect(
+      sharedPage.getByRole('button', { name: 'Cancel' }),
+    ).toBeEnabled()
+    // Select Ministry list
+    await sharedPage
+      .locator('.v-field')
+      .filter({ hasText: 'Ministry/Organization' })
+      .click()
+    const options = sharedPage.locator('[role="listbox"] [role="option"]')
+    await expect(options).toHaveCount(MINISTRIES.length)
+    for (const item of MINISTRIES) {
+      await expect(options.filter({ hasText: item })).toHaveCount(1)
+    }
+    await sharedPage
+      .locator('[role="listbox"] [role="option"]')
+      .filter({ hasText: 'Health' })
+      .click()
+    const tenantDescription = sharedPage.getByLabel('Description of Tenant')
+    await tenantDescription.fill('Test Tenant Description')
+    await sharedPage.getByText('Submit Request').click()
+    await expect(sharedPage.getByText('Success')).toBeVisible()
   })
 })
