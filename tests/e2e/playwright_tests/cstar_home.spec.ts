84c6e55da19ecf5ea3f8942c4d03bc9f097efc32
nimya-aot <nimya.john@aot-technologies.com>
2026-07-30 21:10:19 -0700

    Removed Hard coded Ministry names

--- tests/e2e/playwright_tests/cstar_home.spec.ts
+++ tests/e2e/playwright_tests/cstar_home.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect, Page } from '@playwright/test'
 import { login } from '../support/login'
+import { MINISTRIES } from '../../../frontend/src/utils/constants'
 
 let sharedPage: Page
 
@@ -32,7 +33,8 @@ test.describe.serial('Landing page tests', () => {
   test('Submit tenant request under a Ministry', async () => {
     await sharedPage.getByText('Request a Tenant').click()
     const tenantName = sharedPage.getByLabel('Name of Tenant')
-    await tenantName.fill('Test Tenant')
+    const tenantNameValue = `Test Tenant ${Date.now()}`
+    await tenantName.fill(tenantNameValue)
     await expect(
       sharedPage.locator('[data-testid="button-secondary"]'),
     ).toContainText('Cancel')
@@ -44,47 +46,9 @@ test.describe.serial('Landing page tests', () => {
       .locator('.v-field')
       .filter({ hasText: 'Ministry/Organization' })
       .click()
-    const expectedItems = [
-      'Agriculture and Food',
-      'Attorney General',
-      'BC Elections',
-      'BC Public Service Agency',
-      'Children and Family Development',
-      "Citizens' Services",
-      'Compliance and Enforcement Collaborative',
-      'Corporate Information and Records Management Office',
-      'Crown Agencies and Board Resourcing Office',
-      'Education and Child Care',
-      'Emergency Management and Climate Readiness',
-      'Energy and Climate Solutions',
-      'Environment and Parks',
-      'Finance',
-      'Forests',
-      'Government Communications and Public Engagement',
-      'Health',
-      'Housing and Municipal Affairs',
-      'Indigenous Relations and Reconciliation',
-      'Infrastructure',
-      'Intergovernmental Relations Secretariat',
-      'Jobs, Economic Development and Innovation',
-      'Labour',
-      'Mining and Critical Materials',
-      'Office of the Chief Information Officer',
-      'Office of the Comptroller General',
-      'Office of the Premier',
-      'Public Safety and Solicitor General',
-      'Social Development and Poverty Reduction',
-      'Post-Secondary Education and Future Skills',
-      'Provincial Treasury',
-      "Public Sector Employers' Council Secretariat",
-      'Tourism, Arts, Culture and Sport',
-      'Transportation and Transit',
-      'Treasury Board Staff',
-      'Water, Land and Resource Stewardship',
-    ]
     const options = sharedPage.locator('[role="listbox"] [role="option"]')
-    await expect(options).toHaveCount(expectedItems.length)
-    for (const item of expectedItems) {
+    await expect(options).toHaveCount(MINISTRIES.length)
+    for (const item of MINISTRIES) {
       await expect(options.filter({ hasText: item })).toHaveCount(1)
     }
     await sharedPage
