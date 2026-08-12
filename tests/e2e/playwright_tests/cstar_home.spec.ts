85a0447e77819cc5d7ea755f5abe7cd1a4d50c5e
nimya-aot <nimya.john@aot-technologies.com>
2026-08-12 09:52:43 -0700

    Added approval flow

--- tests/e2e/playwright_tests/cstar_home.spec.ts
+++ tests/e2e/playwright_tests/cstar_home.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect, Page } from '@playwright/test'
 import { login } from '../support/login'
+import { MINISTRIES } from '../../../frontend/src/utils/constants'
 
 let sharedPage: Page
 
@@ -27,5 +28,49 @@ test.describe.serial('Landing page tests', () => {
   test('Checks the navigation links', async () => {
     await expect(sharedPage.getByText('All Tenants')).toBeVisible()
     await expect(sharedPage.getByText('Request a Tenant')).toBeVisible()
+  })
+
+  test('Submit tenant request under a Ministry', async () => {
+    await sharedPage.getByText('Request a Tenant').click()
+    const tenantName = sharedPage.getByLabel('Name of Tenant')
+    const tenantNameValue = `Test Tenant ${Date.now()}`
+    await tenantName.fill(tenantNameValue)
+    await expect(
+      sharedPage.locator('[data-testid="button-secondary"]'),
+    ).toContainText('Cancel')
+    await expect(
+      sharedPage.getByRole('button', { name: 'Cancel' }),
+    ).toBeEnabled()
+    // Select Ministry list
+    await sharedPage
+      .locator('.v-field')
+      .filter({ hasText: 'Ministry/Organization' })
+      .click()
+    const options = sharedPage.locator('[role="listbox"] [role="option"]')
+    await expect(options).toHaveCount(MINISTRIES.length)
+    for (const item of MINISTRIES) {
+      await expect(options.filter({ hasText: item })).toHaveCount(1)
+    }
+    await sharedPage
+      .locator('[role="listbox"] [role="option"]')
+      .filter({ hasText: 'Health' })
+      .click()
+    const tenantDescription = sharedPage.getByLabel('Description of Tenant')
+    await tenantDescription.fill('Test Tenant Description')
+    await sharedPage.getByText('Submit Request').click()
+    await expect(sharedPage.getByText('Success')).toBeVisible()
+    await sharedPage.getByText('settings').click()
+    await sharedPage
+      .locator('tr')
+      .filter({ hasText: tenantNameValue })
+      .locator('div')
+      .filter({ hasText: 'New' })
+      .click()
+    await sharedPage.getByRole('combobox', { name: 'Status' }).click()
+    await sharedPage.getByRole('option', { name: 'APPROVED' }).click()
+    await sharedPage.getByRole('button', { name: 'Submit' }).click()
+    await sharedPage.getByText('All Tenants').click()
+    await expect(sharedPage.getByText(tenantNameValue)).toBeVisible()
+    await sharedPage.getByText(tenantNameValue).click()
   })
 })
