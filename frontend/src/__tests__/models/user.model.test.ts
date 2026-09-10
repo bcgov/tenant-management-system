25d2c3ff52ca91039be359116efbd303e8d548a3
Walter Moar <Walter.Moar@gov.bc.ca>
2026-09-10 07:11:27 -0700

    refactor: CCP-5263 use built-in chip close (#669)

--- frontend/src/__tests__/models/user.model.test.ts
+++ frontend/src/__tests__/models/user.model.test.ts
@@ -25,4 +25,84 @@ describe('User model', () => {
       expect(user.roles).toHaveLength(0)
     })
   })
+
+  describe('getName', () => {
+    it('returns first and last name for an IDIR user', () => {
+      const ssoUser = makeSsoUser({
+        firstName: 'firstName',
+        lastName: 'lastName',
+        idpType: 'idir',
+      })
+      const user = new User({
+        id: toUserId('id'),
+        roles: [],
+        ssoUser,
+      })
+
+      expect(user.getName()).toBe('firstName lastName')
+    })
+
+    it('returns first name only for a BCeID user', () => {
+      const ssoUser = makeSsoUser({
+        firstName: 'firstName',
+        lastName: 'lastName',
+        idpType: 'bceidbusiness',
+      })
+      const user = new User({
+        id: toUserId('id'),
+        roles: [],
+        ssoUser,
+      })
+
+      expect(user.getName()).toBe('firstName')
+    })
+  })
+
+  describe('isBceid', () => {
+    it('returns true for a business BCeID user', () => {
+      const ssoUser = makeSsoUser({ idpType: 'bceidbusiness' })
+      const user = new User({
+        id: toUserId('id'),
+        roles: [],
+        ssoUser,
+      })
+
+      expect(user.isBceid()).toBe(true)
+    })
+
+    it('returns false for an IDIR user', () => {
+      const ssoUser = makeSsoUser({ idpType: 'idir' })
+      const user = new User({
+        id: toUserId('id'),
+        roles: [],
+        ssoUser,
+      })
+
+      expect(user.isBceid()).toBe(false)
+    })
+  })
+
+  describe('isIdir', () => {
+    it('returns true for an IDIR user', () => {
+      const ssoUser = makeSsoUser({ idpType: 'idir' })
+      const user = new User({
+        id: toUserId('id'),
+        roles: [],
+        ssoUser,
+      })
+
+      expect(user.isIdir()).toBe(true)
+    })
+
+    it('returns false for a business BCeID user', () => {
+      const ssoUser = makeSsoUser({ idpType: 'bceidbusiness' })
+      const user = new User({
+        id: toUserId('id'),
+        roles: [],
+        ssoUser,
+      })
+
+      expect(user.isIdir()).toBe(false)
+    })
+  })
 })
